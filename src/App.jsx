f2b74c0c31379d96b8d7d2c56437d5a52720161d
Adam Stewart <adamjoesph11@gmail.com>
2022-09-27 12:47:08 -0500

    adding schedule JSON blob

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  title: "CS Courses for 2018-2019",
+  "courses": {
+    "F101" : {
+      "term": "Fall",
+      "number": "101",
+      "meets" : "MWF 11:00-11:50",
+      "title" : "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110" : {
+      "term": "Fall",
+      "number": "110",
+      "meets" : "MWF 10:00-10:50",
+      "title" : "Intro Programming for non-majors"
+    },
+    "S313" : {
+      "term": "Spring",
+      "number": "313",
+      "meets" : "TuTh 15:30-16:50",
+      "title" : "Tangible Interaction Design and Learning"
+    },
+    "S314" : {
+      "term": "Spring",
+      "number": "314",
+      "meets" : "TuTh 9:30-10:50",
+      "title" : "Tech & Human Interaction"
+    }
+  }
 };
 
 const App = () => {
@@ -14,7 +40,8 @@ const App = () => {
   return (
     <div>
       <h1>{schedule.title}</h1>
-      
+      <Banner/>
+      <CourseList/>
       <p>Today is {day}, {date}.</p>
     </div>
   );
