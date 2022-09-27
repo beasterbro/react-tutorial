b1647deb8b657f9cbca5e8209fd09272ebc2b8b0
Adam Stewart <adamjoesph11@gmail.com>
2022-09-27 13:25:27 -0500

    working on the implementation of CourseList as a component

---
+++ src/components/CourseList.jsx
@@ -0,0 +1,8 @@
+const CourseList = ({courses}) => {  
+    return (
+      <div>
+      </div>
+    );
+  };
+//        <h1>{"<ul><li>" + courses.join('</li><li>') + '</li></ul>'}</h1>
+  export default CourseList;
