ecc9aa6ff51085bd94cc99d8a8bee935f8f01c5c
Adam Stewart <adamjoesph11@gmail.com>
2022-09-30 13:15:29 -0500

    fixing padding issue with CourseItems

--- src/components/CourseItem.jsx
+++ src/components/CourseItem.jsx
@@ -2,7 +2,7 @@ import './CourseItem.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 const CourseItem = ({term, number,meets,title}) => {  
     return (
-        <div className='card ,m-1 p-2'>
+        <div className='card m-1 p-2'>
           <h4 className='card-title'>{term} CS {number}</h4>
           <div className='card-body'>
             <p className='card-text'>{title}</p>
