7973f469a936e8086c42c6913cff0ae5115ce263
Adam Stewart <adamjoesph11@gmail.com>
2022-09-30 12:31:00 -0500

    changing formatting to match given example for list of courses

--- src/components/CourseItem.jsx
+++ src/components/CourseItem.jsx
@@ -2,7 +2,7 @@
 const CourseListItem = ({term, number,meets,title}) => {  
     return (
         <div>
-      <li> {[term, number, meets, title].join(' ')} </li>
+      <li> {term} CS {number}: {title} </li>
       </div>
     );
   };
