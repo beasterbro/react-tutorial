73c1e0829c013e3d2979a0394755a7e0158dd81d
Adam Stewart <adamjoesph11@gmail.com>
2022-10-10 18:32:15 -0500

    COLOR

--- src/components/CourseItem.jsx
+++ src/components/CourseItem.jsx
@@ -5,7 +5,7 @@ const CourseItem = ({course, selectedCourses, toggleSelected}) => {
     return (//TODO: add toggle selected
         <div className='card m-1 p-2' onClick={() => toggleSelected(course.number)} >
           <h4 className='card-title'>{course.term} CS {course.number}</h4>
-          <div className={`card-body ${selectedCourses.includes(course.id) ? 'selected' : ''}`}>
+          <div className={`card-body ${selectedCourses.includes(course.number) ? 'selected' : ''}`}>
             <p className='card-text'>{course.title}</p>
             {selectedCourses.includes(course.number) && <p>selected</p>}
             <p className='card-footer bg-transparent'>{course.meets}</p>
