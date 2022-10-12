a391625884a4359659d4398a452b6ecc6e85e18f
Adam Stewart <adamjoesph11@gmail.com>
2022-10-11 19:51:56 -0500

    fixing formatting

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -8,9 +8,11 @@ return (
       selectedCourses.length === 0
       ? <h2>The list is empty</h2>
       : selectedCourses.map(course => (
+        <ul>
           <div key={course.number}>
             {course.title} -- {course.meets}
           </div>
+          </ul>
         ))
     }
   </div>
