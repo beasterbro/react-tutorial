c57700ce65a22b94a531744318ec5879a9cd5da6
Adam Stewart <adamjoesph11@gmail.com>
2022-10-17 13:45:08 -0500

    adding firebase storage to project

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,54 +1,53 @@
-// import { useEffect, useState } from 'react';
-// import { getDatabase, onValue, ref, update} from 'firebase/database';
-// ...
-// const database = getDatabase(firebase);
+import { useEffect, useState } from 'react';
+import { getDatabase, onValue, ref, update} from 'firebase/database';
+import { useCallback, useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, update } from 'firebase/database';
 
-// import { useCallback, useEffect, useState } from 'react';
-// import { initializeApp } from "firebase/app";
-// import { getDatabase, onValue, ref, update } from 'firebase/database';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCqy8l97tEWjvW3V1B0f9bMsMLFk9D4sWk",
-//   authDomain: "maxtactoe.firebaseapp.com",
-//   databaseURL: "https://maxtactoe.firebaseio.com",
-//   projectId: "maxtactoe",
-//   storageBucket: "maxtactoe.appspot.com",
-//   messagingSenderId: "672040841619",
-//   appId: "1:672040841619:web:e488e188d5b93db7753866"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyD5YOnCIzZn3QXvuYo3y4H_udO8L-Q0REg",
+    authDomain: "react-tutorial-5d87a.firebaseapp.com",
+    databaseURL: "https://react-tutorial-5d87a-default-rtdb.firebaseio.com",
+    projectId: "react-tutorial-5d87a",
+    storageBucket: "react-tutorial-5d87a.appspot.com",
+    messagingSenderId: "109768872358",
+    appId: "1:109768872358:web:c961754ff94ad4c2247f67",
+    measurementId: "G-M4MBZ9QLWT"
+  };
 
-// // Initialize Firebase
-// const firebase = initializeApp(firebaseConfig);
-// const database = getDatabase(firebase);
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
 
-// export const useDbData = (path) => {
-//   const [data, setData] = useState();
-//   const [error, setError] = useState(null);
+export const useDbData = (path) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
 
-//   useEffect(() => (
-//     onValue(ref(database, path), (snapshot) => {
-//      setData( snapshot.val() );
-//     }, (error) => {
-//       setError(error);
-//     })
-//   ), [ path ]);
+  useEffect(() => (
+    onValue(ref(database, path), (snapshot) => {
+     setData( snapshot.val() );
+    }, (error) => {
+      setError(error);
+    })
+  ), [ path ]);
 
-//   return [ data, error ];
-// };
+  return [ data, error ];
+};
 
-// const makeResult = (error) => {
-//   const timestamp = Date.now();
-//   const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
-//   return { timestamp, error, message };
-// };
+const makeResult = (error) => {
+  const timestamp = Date.now();
+  const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+  return { timestamp, error, message };
+};
 
-// export const useDbUpdate = (path) => {
-//   const [result, setResult] = useState();
-//   const updateData = useCallback((value) => {
-//     update(ref(database, path), value)
-//     .then(() => setResult(makeResult()))
-//     .catch((error) => setResult(makeResult(error)))
-//   }, [database, path]);
+export const useDbUpdate = (path) => {
+  const [result, setResult] = useState();
+  const updateData = useCallback((value) => {
+    update(ref(database, path), value)
+    .then(() => setResult(makeResult()))
+    .catch((error) => setResult(makeResult(error)))
+  }, [database, path]);
 
-//   return [updateData, result];
-// };
+  return [updateData, result];
+};
