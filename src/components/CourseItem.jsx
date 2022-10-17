a2f61585f014ef277724f561084f99af081823e4
Adam Stewart <adamjoesph11@gmail.com>
2022-10-16 22:53:06 -0500

    adding edit button

--- src/components/CourseItem.jsx
+++ src/components/CourseItem.jsx
@@ -3,14 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { catchTimeConflicts } from '../utilities/timing';
 const CourseItem = ({course, selectedCourses, toggleSelected}) => { 
     return (//TODO: add toggle selected
-        <div className='card m-1 p-2' onClick={() => toggleSelected(course)} >
+    <div className='card m-1 p-2'>
+        <div onClick={() => toggleSelected(course)} >
           <h4 className='card-title'>{course.term} CS {course.number}</h4>
           <div className={`card-body ${selectedCourses.includes(course) ? 'selected' : ''}`}>
             <p className='card-text'>{course.title}</p>
             {selectedCourses.includes(course) && <p>selected</p>}
             <p className='card-footer bg-transparent'>{course.meets}</p>
           </div>
-      </div>//on click toggle selected
+      </div>
+      <button>Edit</button>
+      </div>
     );
   };
 
